b287cb9018b64ed63be207f98955b27deb5e9fee
Asil Kaan Bozcuoglu <asil@cs.uni-bremen.de>
2020-12-10 22:07:02 +0100

    my implementation of c++ pid controller

--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,7 @@ int main() {
   uWS::Hub h;
 
   PID pid;
+  pid.Init(0.11, 0.0018, 2.4);
   /**
    * TODO: Initialize the pid variable.
    */
@@ -63,7 +64,8 @@ int main() {
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
-          
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
                     << std::endl;
